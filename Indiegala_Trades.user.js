25dae02a1cd7ebc9f5e4e57b8b63df16b6327196
l4ll3 <lallemailfrom+github.com@gmail.com>
2017-01-18 19:12:36 +0100

    Fix Icon placement. Bugfix for undefined platform in cache hit

--- Indiegala_Trades.user.js
+++ Indiegala_Trades.user.js
@@ -11,58 +11,61 @@
 // ==/UserScript==
 
 $("head link:last")
-.after("<link rel=stylesheet type=text/css href=https://steamstore-a.akamaihd.net/public/css/v6/store.css>")
-.after("<style>span.platform_img {background-color: black; height:20px; display: inline-block; opacity: 0.90;} span.lalle__platform {position: relative; bottom: 20px; right: 0px;}</style>");
-
+    .after("<link rel=stylesheet type=text/css href=https://steamstore-a.akamaihd.net/public/css/v6/store.css>")
+    .after("<style>span.platform_img {background-color: black; height:20px; display: inline-block; opacity: 0.90;} span.lalle__platform {position: relative; bottom: 23px; right: 0px;}</style>");
 
 function setPlatformSpan(node, steamlink) {
     if (!steamlink.length) {
-      return;
+        return;
     }
     var platformhtml = "";
     if (simpleStorage.canUse()) {
-      if(simpleStorage.hasKey(steamlink)) {
-        platformhtml = simpleStorage.get(steamlink);
-      }
+        if(simpleStorage.hasKey(steamlink)) {
+            platformhtml = simpleStorage.get(steamlink);
+        }
     } else {
-      console.warn("Unable to use local storage. Retrieving steam platform will be slow!");
+        console.warn("Unable to use local storage. Retrieving steam platform will be slow!");
     }
-  
-    if (platformhtml.length)  {
-      node.append("<a href=\""+ steamlink + "\"><span class=\"lalle__platform\">" + platformhtml + "</span></a>");
+
+    console.log("platformhtml: " + platformhtml);
+
+    if (platformhtml !== undefined && platformhtml.length)  {
+        node.append("<a href=\""+ steamlink + "\"><span class=\"lalle__platform\">" + platformhtml + "</span></a>");
+        console.log("Cache append: " + platformhtml);
     }
     else {
-      GM_xmlhttpRequest({
-        method: "GET",
-        url: steamlink,
-        onload: function(response) {
-          var platform = $(response.responseText).find("div.game_area_purchase_platform");
-          if (platform.length) {
-            if (simpleStorage.canUse())  {
-              simpleStorage.set(steamlink, platform.html(), {TTL: 2592000000})
+        GM_xmlhttpRequest({
+            method: "GET",
+            url: steamlink,
+            onload: function(response) {
+                var platform = $(response.responseText).find("div.game_area_purchase_platform");
+                if (platform.length) {
+                    if (simpleStorage.canUse())  {
+                        simpleStorage.set(steamlink, platform.html(), {TTL: 2592000000});
+                    }
+                    node.append("<a href=\""+ steamlink + "\"><span class=\"lalle__platform\">" + platform.html() + "</span></a>");
+                    console.log("Req Append: " + platform.htlm());
+                }
             }
-            node.append("<a href=\""+ steamlink + "\"><span class=\"lalle__platform\">" + platform.html() + "</span></a>");
-          }
-        }
-      });
-    }  
+        });
+    }
 }
 
 function getSteamLink(trade) {
-  var gameImg = trade.find("img:first");
-  var steamapp = gameImg.attr("src").replace("http://cdn.akamai.steamstatic.com/steam/apps/", "").replace("/header.jpg", "");
-  var steamlink = "http://store.steampowered.com/app/" + steamapp + "/";  
-  return steamlink;
+    var gameImg = trade.find("img:first");
+    var steamapp = gameImg.attr("src").replace("http://cdn.akamai.steamstatic.com/steam/apps/", "").replace("/header.jpg", "");
+    var steamlink = "http://store.steampowered.com/app/" + steamapp + "/";
+    return steamlink;
 }
 
 $("div.trade-cont").each(function () {
-  var trade = $(this);
-  var steamlink = getSteamLink(trade);
-  setPlatformSpan(trade.find("div.trade_img"), steamlink);
+    var trade = $(this);
+    var steamlink = getSteamLink(trade);
+    setPlatformSpan(trade.find("div.trade_img").find("p:first"), steamlink);
 });
 
 $("div.trade-img-cont").each(function() {
-  var trade = $(this);
-  var steamlink = getSteamLink(trade);
-  setPlatformSpan(trade, steamlink);
+    var trade = $(this);
+    var steamlink = getSteamLink(trade);
+    setPlatformSpan(trade, steamlink);
 });
